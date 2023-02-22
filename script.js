93a387e7aac05d0a795cadc916550b8c907c560c
Lucie Sundermeijer <lucie.sun@gmail.com>
2023-02-22 18:47:21 +0100

    add mobile versionl

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let formEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-
 
 const userEmail = document.getElementById("email")
 let errorMessage = document.querySelector(".email-error-msg")
+let errorIcon = document.querySelector(".email-error-icon")
 
 //actions when user click on submit button
 const btn = document.querySelector('.email-form-button')
@@ -11,13 +12,17 @@ btn.addEventListener('click', (e) => {
     checkEmail()
 })
 
-//check if email is valid and if not display an error message
+//check if email is valid and if not display the error message
 function checkEmail(){
     if (!formEmail.test(userEmail.value)){
-        errorMessage.classList.add('active')
+        errorMessage.style.display='block'
+        userEmail.classList.add('error')
+        errorIcon.style.display='inline-block'
     }
     else{
-        errorMessage.classList.remove('active')
+        errorMessage.style.display='none'
+        userEmail.classList.remove('error')
+        errorIcon.style.display='none'
     }
 }
 
